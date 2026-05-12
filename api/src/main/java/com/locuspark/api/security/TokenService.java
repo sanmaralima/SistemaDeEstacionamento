755158b3789d995c2ce06941a6779db0c175bd66
me-lucas-al <lucasalsouza2006@gmail.com>
2026-05-12 01:07:29 -0300

    fix(timezone): resolve time zone token expiration error

--- api/src/main/java/com/locuspark/api/security/TokenService.java
+++ api/src/main/java/com/locuspark/api/security/TokenService.java
@@ -46,6 +46,6 @@ public class TokenService {
     }
 
     private Instant generateExpirationDate() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.UTC); 
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
